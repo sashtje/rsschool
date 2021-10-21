b66a383aea74e41e7b288621e8e5ec340b937d71
sashtje <sashtje.lefevre@gmail.com>
2021-10-21 15:45:17 +0300

    feat: add showDate

--- momentum/js/watch-calendar.js
+++ momentum/js/watch-calendar.js
@@ -1,10 +1,20 @@
-const time = document.querySelector(".time");
+const timeTag = document.querySelector(".time");
+const dateTag = document.querySelector(".date");
+
+function showDate(date, locales) {
+  const options = { weekday: "long", day: "numeric", month: "long" };
+  let dateText = date.toLocaleDateString(locales, options);
+  dateTag.textContent = dateText[0].toUpperCase() + dateText.slice(1);
+}
 
 function showTime() {
   const date = new Date();
-  const currentTime = date.toLocaleTimeString("ru-RU", { hour12: false });
-  time.textContent = currentTime;
+  const options = { hour12: false };
+  const locales = "ru-RU";
   /* 'en-US' */
+  const currentTime = date.toLocaleTimeString(locales, options);
+  timeTag.textContent = currentTime;
+  showDate(date, locales);
 
   setTimeout(showTime, 1000);
 }
