ad58748b53532fd3cc26252e41936b4064b07f51
sashtje <sashtje.lefevre@gmail.com>
2021-11-14 15:20:25 +0300

    feat: add base layout and styles for main-page

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -5,6 +5,7 @@ import Game from "./scripts/game.js";
 import CategoryArtist from "./scripts/category-artist.js";
 import CategoryPicture from "./scripts/category-picture.js";
 import Results from "./scripts/results.js"; */
+import "./scss/main.scss";
 
 /* create full App */
 const app = new App();
