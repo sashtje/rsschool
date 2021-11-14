7152590e0b107062165db43378c607d45b830803
sashtje <sashtje.lefevre@gmail.com>
2021-11-14 20:08:31 +0300

    feat: add downloadSettings() and writeSettingsToLocalStorage() for Settings class

--- art-quiz/src/scripts/settings.js
+++ art-quiz/src/scripts/settings.js
@@ -8,8 +8,30 @@ export class Settings {
   durationTime = 5;
 
   constructor() {
-    /* download from localStorage if there are */
+    downloadSettings();
   }
 
-  writeSettingsToLocalStorage() {}
+  downloadSettings() {
+    /* download from localStorage if there are */
+    if (localStorage.getItem("settings")) {
+      let savedSettings = JSON.parse(localStorage.getItem("settings"));
+
+      for (let key of Object.keys(savedSettings)) {
+        this[key] = savedSettings[key];
+      }
+    }
+  }
+
+  writeSettingsToLocalStorage() {
+    localStorage.setItem(
+      "settings",
+      JSON.stringify({
+        lang: this.lang,
+        sound: this.sound,
+        volumeSound: this.volumeSound,
+        time: this.time,
+        durationTime: this.durationTime,
+      })
+    );
+  }
 }
