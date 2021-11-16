b8dd466012eb57021f4716b9d331cb855f46b27f
sashtje <sashtje.lefevre@gmail.com>
2021-11-16 17:01:37 +0300

    feat: add listeners for settings page

--- art-quiz/src/scripts/app.js
+++ art-quiz/src/scripts/app.js
@@ -8,6 +8,7 @@ export class App {
   dataImages = [];
   currPageType = consts.HOME;
   appSwitcher = {};
+  wereAlreadyOpened = [];
 
   constructor() {
     this.appSwitcher = new Switcher();
@@ -27,7 +28,8 @@ export class App {
     this.appSwitcher.switchPage(this.currPageType, consts.SETTINGS);
     this.currPageType = consts.SETTINGS;
 
-    this.addListenersForSettingsPage();
+    if (!this.wereAlreadyOpened.includes("settings"))
+      this.addListenersForSettingsPage();
   };
 
   returnToHomePage = (e) => {
@@ -58,10 +60,11 @@ export class App {
       circle.remove();
       this.appSwitcher.switchPage(this.currPageType, consts.HOME);
       this.currPageType = consts.HOME;
-    }, 200);
+    }, 100);
   };
 
   addListenersForMainPage() {
+    this.wereAlreadyOpened.push("main-page");
     let btnSettings = document.querySelector(".main-page__btn-settings");
 
     btnSettings.addEventListener("click", this.openSettingsPage);
@@ -80,10 +83,73 @@ export class App {
   }
 
   addListenersForSettingsPage() {
+    this.wereAlreadyOpened.push("settings");
     //handle for save button
     let saveBtn = document.querySelector(".settings__btn-save");
 
     saveBtn.addEventListener("click", this.returnToHomePage);
+
+    let chkbxLang = document.querySelector(".settings-lang__checkbox");
+    let chkbxSound = document.querySelector(".settings-sound__checkbox");
+    let chkbxTime = document.querySelector(".settings-time__checkbox");
+    let volumeRange = document.querySelector(".settings-sound__range");
+    let timeRange = document.querySelector(".settings-time__range");
+    let timeRangeVal = document.querySelector(".settings-time__time-value");
+
+    chkbxLang.addEventListener("change", () => {
+      this.appSwitcher.translateSettingsPage();
+    });
+
+    chkbxSound.addEventListener("change", () => {
+      if (chkbxSound.checked) {
+        volumeRange.disabled = false;
+      } else {
+        volumeRange.disabled = true;
+        volumeRange.style.background = "";
+        volumeRange.value = 0;
+      }
+    });
+
+    chkbxTime.addEventListener("change", () => {
+      if (chkbxTime.checked) {
+        timeRange.disabled = false;
+      } else {
+        timeRange.disabled = true;
+        timeRange.style.background = "";
+        timeRange.value = 5;
+        timeRangeVal.textContent = "5";
+      }
+    });
+
+    volumeRange.addEventListener("input", function () {
+      let value = this.value;
+
+      this.style.background =
+        "linear-gradient(to right, #b14c00 0%, #b14c00 " +
+        value * 100 +
+        "%, #6d6d6d " +
+        value * 100 +
+        "%, #6d6d6d 100%)";
+    });
+
+    timeRange.addEventListener("input", function () {
+      let value = this.value - 5;
+      if (value !== 0 && value !== 25) {
+        let coef = value / 5;
+        value = (46 + 36 * (coef - 1)) / 2;
+      } else {
+        value = value * 4;
+      }
+
+      this.style.background =
+        "linear-gradient(to right, #b14c00 0%, #b14c00 " +
+        value +
+        "%, #6d6d6d " +
+        value +
+        "%, #6d6d6d 100%)";
+
+      timeRangeVal.textContent = this.value;
+    });
   }
 
   async downloadDataImages() {
