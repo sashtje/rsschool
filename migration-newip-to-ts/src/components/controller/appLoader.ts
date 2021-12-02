0e073d092b4e0663f2a0200bcd4fbe36c42e7f7d
sashtje <sashtje.lefevre@gmail.com>
2021-12-02 13:12:45 +0300

    feat: change tsconfig and eslintrc

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'd95e349eddc04386b5c2f246bb35ca20', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'd95e349eddc04386b5c2f246bb35ca20', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
