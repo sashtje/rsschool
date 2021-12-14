d9051a60972978175c13c3e7be406b09cd228118
sashtje <sashtje.lefevre@gmail.com>
2021-12-14 12:49:41 +0300

    feat: add preloader for the christmas app

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello!');
+import './scss/all-styles.scss';
+
+function turnOffPreloader(): void {
+  const preloader: HTMLElement = document.querySelector('.preloader') as HTMLElement;
+
+  function handlePreloaderTransitionEnd(): void {
+    preloader.style.display = 'none';
+    preloader.classList.remove('preloader_is-hiding');
+    preloader.removeEventListener('transitionend', handlePreloaderTransitionEnd);
+  }
+
+  preloader.addEventListener('transitionend', handlePreloaderTransitionEnd);
+  preloader.classList.add('preloader_is-hiding');
+}
+
+turnOffPreloader();
